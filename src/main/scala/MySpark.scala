73aee8aa6b4e393e474f5e69c7bd1a4a0b27ecf6
sunupping <thesuninmyheart@126.com>
2018-07-30 16:35:49 +0800

    简单案例

---
+++ src/main/scala/MySpark.scala
@@ -0,0 +1,32 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by jie.sun on 2018/7/30.
+  */
+object MySpark {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("mySpark")
+    //setMaster("local") 本机的spark就用local，远端的就写ip
+    //如果是打成jar包运行则需要去掉 setMaster("local")因为在参数中会指定。
+    conf.setMaster("local")
+    val sc = new SparkContext(conf)
+    val data = Array(1,2,3,4,5,6)
+    val distData = sc.parallelize(data) //sc.parallelize(data,10) 第二个参数代表分区数，每个分区会对应一个任务
+
+
+   // val rdd = sc.parallelize(List(1,2,3,4,5,6).map(_*3))
+   // val mappedRDD = rdd.filter(_>10).collect()
+
+    //对集合求和
+    println(distData.reduce(_+_)*3)
+    //输出大于10的元素
+//    for(arg <- mappedRDD){
+//      print(arg+" ")
+//    }
+    println()
+    println("math is work")
+
+
+  }
+}
