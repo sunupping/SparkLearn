497ba0e199cd526f56ca05916ff2ebddf8ee4ec4
sunupping <thesuninmyheart@126.com>
2018-07-30 17:55:15 +0800

    简单案例更新

--- src/main/scala/MySpark.scala
+++ src/main/scala/MySpark.scala
@@ -12,14 +12,20 @@ object MySpark {
     conf.setMaster("local")
     val sc = new SparkContext(conf)
     val data = Array(1,2,3,4,5,6)
+    //使用累加器方法来避免
+    val accum = sc.longAccumulator("Ede Accumulator")
     val distData = sc.parallelize(data) //sc.parallelize(data,10) 第二个参数代表分区数，每个分区会对应一个任务
-
+//    var counter = 0
+    distData.foreach(x => {
+      accum.add(x)
+    })
+    println("Counter value: "+accum.value)
 
    // val rdd = sc.parallelize(List(1,2,3,4,5,6).map(_*3))
    // val mappedRDD = rdd.filter(_>10).collect()
 
     //对集合求和
-    println(distData.reduce(_+_)*3)
+    //println(distData.reduce(_+_)*3)
     //输出大于10的元素
 //    for(arg <- mappedRDD){
 //      print(arg+" ")
